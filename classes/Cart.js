d4c35b8e9426a58dfd3a9794f1da0124ec832bfd
Dhamu2955 <Dharmendra.Pindolia@gmail.com>
2024-09-30 14:27:47 +0100

    done stretch 1 and 2

--- classes/Cart.js
+++ classes/Cart.js
@@ -11,4 +11,19 @@ module.exports = class Cart {
         this.total -= this.products[index].price
         this.products.splice(index, 1)
     }
+    // getTotal(){
+    //     let total = 0;
+    //     for (let i = 0; i < this.products.length; i++){
+    //         total += this.products[i].price
+    //     }
+    //     return total
+    // }
+    getTotal(){
+        return this.total
+    }
+
+    clear(){
+        this.total = 0
+        this.products = []
+    }
 }
